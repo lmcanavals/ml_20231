411077d9e84ac9a698d4e242ebb5b1ed067e2664
Luis M Canaval S <public02@canaval.org>
2023-06-19 14:06:48 -0500

    without timeout but with auto resume

--- 1381_off_to_binvox.sh
+++ 1381_off_to_binvox.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
-curl -O https://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip
-unzip -q ModelNet10.zip
-du -sh ModelNet10
-ls -l ModelNet10
-curl -O https://raw.githubusercontent.com/ChristianEspirituCueva/ML_RestorationGAN/main/binvox
-chmod +x binvox
-mkdir -p test
-mkdir -p train
-cd train
-for directory in ../ModelNet10/*; do
-    if [[ -d $directory ]]; then
-        for f in $directory/train/*; do
-            mv $f .
-        done
-    fi
-done
+dothething() {
+	if [[ ! -f ModelNet10.zip ]]; then
+		curl -O https://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip
+		unzip -q ModelNet10.zip
+		du -sh ModelNet10
+		ls -l ModelNet10
+		mkdir -p test
+		mkdir -p train
+		cd train
+		for directory in ../ModelNet10/*; do
+			if [[ -d $directory ]]; then
+				for f in $directory/train/*; do
+					mv $f .
+				done
+			fi
+		done
+		cd ..
 
-cd ../test
-for directory in ../ModelNet10/*; do
-    if [[ -d $directory ]]; then
-        for f in $directory/test/*; do
-            mv $f .
-        done
-    fi
-done
+		cd test
+		for directory in ../ModelNet10/*; do
+			if [[ -d $directory ]]; then
+				for f in $directory/test/*; do
+					mv $f .
+				done
+			fi
+		done
+		cd ..
+	fi
 
-for f in *; do
-		echo "Processing $f"
-    ../binvox -pb -d 32 $f &> /dev/null
-done
-cd ../train
-for f in *; do
-		echo "Processing $f"
-    ../binvox -pb -d 32 $f &> /dev/null
-done
+	if [[ ! -f binvox ]]; then
+		curl -O https://raw.githubusercontent.com/ChristianEspirituCueva/ML_RestorationGAN/main/binvox
+		chmod +x binvox
+	fi
+
+	cd test
+	for f in *; do
+		bvfile="${f/off/binvox}"
+		if [[ ! -f $bvfile ]]; then
+			echo "Processing $f"
+			../binvox -pb -d 32 $f &> /dev/null
+		fi
+	done
+	cd ..
+
+	cd train
+	for f in *; do
+		bvfile="${f/off/binvox}"
+		if [[ ! -f $bvfile ]]; then
+			echo "Processing $f"
+			../binvox -pb -d 32 $f &> /dev/null
+		fi
+	done
+	cd ..
+}
+dothething
